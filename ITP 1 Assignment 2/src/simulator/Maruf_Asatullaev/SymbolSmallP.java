433d362fece9675156d79ec5f2728f6183a3d90a
Maruco <64041220+Maruf-co@users.noreply.github.com>
2020-11-14 22:50:59 +0500

    Add files via upload

--- ITP 1 Assignment 2/src/simulator/Maruf_Asatullaev/SymbolSmallP.java
+++ ITP 1 Assignment 2/src/simulator/Maruf_Asatullaev/SymbolSmallP.java
@@ -1,9 +1,6 @@
 package simulator.Maruf_Asatullaev;
 
-import simulator.do_not_change.Aggressive;
-import simulator.do_not_change.Position;
-import simulator.do_not_change.SmallCase;
-import simulator.do_not_change.Symbol;
+import simulator.do_not_change.*;
 
 import java.util.Random;
 
@@ -19,14 +16,14 @@ public class SymbolSmallP extends Symbol implements Aggressive, SmallCase {
     public void move() {
         Random rand = new Random();
         int randNumber = rand.nextInt(3);
-        if(randNumber == 0 && this.position.column != 0){ // Left
-            this.position.column -= 1;
-        }else if(randNumber == 1 && this.position.column != 9){ // Right
-            this.position.column += 1;
-        }else if(randNumber == 2 && this.position.row != 0){ // Up
-            this.position.row -= 1;
+        if(randNumber == 0 && position.column != 0){ // Left
+            position = new Position(position.column-1, position.row);
+        }else if(randNumber == 1 && position.column != 9){ // Right
+            position = new Position(position.column+1, position.row);
+        }else if(randNumber == 2 && position.row != 0){ // Up
+            position = new Position(position.column, position.row-1);
         }else{ // Down
-            this.position.row += 1;
+            position = new Position(position.column, position.row+1);
         }
     }
 
@@ -37,11 +34,25 @@ public class SymbolSmallP extends Symbol implements Aggressive, SmallCase {
 
     @Override
     public void upgrade() {
-
+        Symbol tmp = new SymbolCapitalP(Simulator.id++, position);
+        WorldController.world.get(position).add(tmp);
     }
     @Override
     public void attackSmart() {
-
+        for (int i = 1; i <= sightDistance; i++) {
+            if(position.row + i < 10 &&
+                    (WorldController.world.get(position.row + i).get(0) instanceof SymbolCapitalR ||
+                            WorldController.world.get(position.row + i).get(0) instanceof SymbolSmallR)){
+                position = new Position(position.column, position.row+1);
+                break;
+            }
+            if(position.row - i > 0 &&
+                    (WorldController.world.get(position.row - i).get(0) instanceof SymbolCapitalR ||
+                            WorldController.world.get(position.row - i).get(0) instanceof SymbolSmallR)){
+                position = new Position(position.column, position.row-1);
+                break;
+            }
+        }
     }
 
 }
